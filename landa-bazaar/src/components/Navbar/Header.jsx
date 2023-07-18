240a47fd382c7ce1345d73aae2bea9a50ea2a8a8
Waqas7616 <waqasali761611@gmail.com>
2023-07-18 17:51:27 +0500

    Navbar created

---
+++ landa-bazaar/src/components/Navbar/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import './Header.css'
+import logo from '../../images/logo.png'
+function Header() {
+  return (
+    <Navbar expand="lg" className="bg-light">
+      <Container>
+        <Navbar.Brand href="#home"><img src={logo} alt="" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Shop</Nav.Link>
+            <Nav.Link href="#link">Collection</Nav.Link>
+            <Nav.Link href="#link">About Us</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
